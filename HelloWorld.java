14ce75e6cfe9a508d7fbd563625e41672454e9f9
Tom <52895819+tomphillipsmusic@users.noreply.github.com>
2020-02-27 15:15:30 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World & Welcome to Jenkins training");
+		System.out.println("Made some changes");
 	}
 
 }
